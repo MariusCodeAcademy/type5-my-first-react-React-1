1a50cff4a39e168dc0b8ea65e3bcc90834953fee
Marius <marius@example.com>
2022-03-25 13:51:08 +0200

    travelData from array

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,13 +1,43 @@
 import TravelCard from './TravelCard';
 import './TravelList.css';
 
+const travelData = [
+  {
+    title: 'New York',
+    image: 'img/newyork.jpg',
+    date: 'Fri 27 Nov 2016',
+  },
+  {
+    title: 'Paris',
+    image: 'img/paris.jpg',
+    date: 'Fri 27 Nov 2021',
+  },
+  {
+    title: 'San Francisco',
+    image: 'img/sanfran.jpg',
+    date: 'Fri 17 Nov 2022',
+  },
+];
+
 function TravelList() {
   return (
     <div className='travel'>
       <div className='container grid'>
-        <TravelCard title='New York' image='img/newyork.jpg' date='Fri 27 Nov 2016' />
-        <TravelCard title='Paris' image='img/paris.jpg' date='Fri 20 Nov 2019' />
-        <TravelCard title='San Francisco' image='img/sanfran.jpg' date='Fri 17 Nov 2020' />
+        <TravelCard
+          title={travelData[0].title}
+          image={travelData[0].image}
+          date={travelData[0].date}
+        />
+        <TravelCard
+          title={travelData[1].title}
+          image={travelData[1].image}
+          date={travelData[1].date}
+        />
+        <TravelCard
+          title={travelData[2].title}
+          image={travelData[2].image}
+          date={travelData[2].date}
+        />
       </div>
     </div>
   );
