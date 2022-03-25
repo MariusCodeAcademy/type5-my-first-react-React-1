34387d17c1b1c30fa961150a5f2e61037d863661
Marius <marius@example.com>
2022-03-25 13:36:13 +0200

    TravelCard props

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,19 +1,17 @@
 import BlackBtn from './BlackBtn';
 
 const data = {
-  image: 'img/newyork.jpg',
-  title: 'New York',
-  date: 'Fri 27 Nov 2016',
   para: 'Praesent tincidunt sed tellus ut rutrum sed vitae justo.',
 };
 
-function TravelCard() {
+function TravelCard(props) {
+  // console.log('props ===', props);
   return (
     <div className='travel-card'>
-      <img className='travel-image' src={data.image} alt='town' />
+      <img className='travel-image' src={props.image} alt='town' />
       <div className='travel-info'>
-        <h3>{data.title}</h3>
-        <p className='date'>{data.date}</p>
+        <h3>{props.title}</h3>
+        <p className='date'>{props.date}</p>
         <p>{data.para}</p>
         <BlackBtn />
       </div>
--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -5,7 +5,9 @@ function TravelList() {
   return (
     <div className='travel'>
       <div className='container grid'>
-        <TravelCard />
+        <TravelCard title='New York' image='img/newyork.jpg' date='Fri 27 Nov 2016' />
+        <TravelCard title='Paris' image='img/paris.jpg' date='Fri 20 Nov 2019' />
+        <TravelCard title='San Francisco' image='img/sanfran.jpg' date='Fri 17 Nov 2020' />
       </div>
     </div>
   );
